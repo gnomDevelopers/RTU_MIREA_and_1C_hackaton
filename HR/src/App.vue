ddeb9df8d7a04dd48c7e4eb142767adb1398b5a7
unwelcome <moyapohtaxd@mail.ru>
2024-11-21 19:26:54 +0300

    создан фронтенд для HR VUZ+

---
+++ HR/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <RouterView />
+</template>
+<script lang="ts">
+export default{
+  
+};
+</script>
---
+++ HR/src/pages/WelcomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col w-full items-center">
+    <div class="flex flex-col w-10/12 items-center">
+      <p class="text-3xl font-medium underline">Welcome Page</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default{
+  
+};
+</script>
