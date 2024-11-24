1046047802a28c54a12ff4c413c9d4bd43182752
unwelcome <moyapohtaxd@mail.ru>
2024-11-24 20:41:06 +0300

    фикс qr reader

--- web/src/pages/ScanQRPage.vue
+++ web/src/pages/ScanQRPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center scrollable px-4 lg:px-0">
     <div class="flex flex-col w-full lg:w-10/12 items-center gap-y-4 mb-4">
-      <QrcodeStream @decode="onDecode"/>
+      <QrcodeStream @detect="onDecode"/>
       <p>Статус: {{ status }} Расшифровано: {{ decoded }}</p>
     </div>
   </div>
