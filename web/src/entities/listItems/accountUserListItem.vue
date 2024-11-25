9bc5bf9bc9a7172cfbb3f2f070a64d14639cb36b
unwelcome <moyapohtaxd@mail.ru>
2024-11-25 21:20:12 +0300

    фикс создания пользователя, добавлена смена ролей

--- web/src/entities/listItems/accountUserListItem.vue
+++ web/src/entities/listItems/accountUserListItem.vue
@@ -20,15 +20,17 @@
           </option>
 
         </select>
-      <div v-if="userRole !== data.role" class="py-1 px-2 rounded-lg cursor-pointer text-base select-none btn">Сохранить</div>
-      <div class="py-1 px-2 rounded-lg cursor-pointer text-base select-none btn btn-bad">{{ data.role !== 6 ? 'Уволить' : 'Отчислить' }}</div>
+      <div v-if="userRole !== data.role" @click="updateUser" class="py-1 px-2 rounded-lg cursor-pointer text-base select-none btn">Сохранить</div>
+      <div @click="deleteUser" class="py-1 px-2 rounded-lg cursor-pointer text-base select-none btn btn-bad">{{ data.role !== 6 ? 'Уволить' : 'Отчислить' }}</div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { mapStores } from 'pinia';
 import { useUserInfoStore } from '@/stores/userInfoStore';
-import { ROLES_NAME, ROLES_SET_PRORECTOR, ROLES_SET_DECAN_TO_PREPOD, type IUserGet } from '@/helpers/constants';
+import { useUniversityStore } from '@/stores/universityStore';
+import { useStatusWindowStore } from '@/stores/statusWindowStore';
+import { ROLES_NAME, ROLES_SET_PRORECTOR, ROLES_SET_DECAN_TO_PREPOD, type IUserGet, StatusCodes } from '@/helpers/constants';
 import type { PropType } from 'vue';
 export default{
   props:{
@@ -44,12 +46,56 @@ export default{
     }
   },
   computed:{
-    ...mapStores(useUserInfoStore),
+    ...mapStores(useUserInfoStore, useUniversityStore, useStatusWindowStore),
 
   },
   methods: {
     getRolesName(roleID: number): string{
       return ROLES_NAME[roleID];
+    },
+    getUserList(role: number){
+      switch (role){
+        case 2: return this.universityStore.decansList;
+        case 3: return this.universityStore.educationDepartmentsList;
+        case 4: return this.universityStore.zavCafsList;
+        case 5: return this.universityStore.teachersList;
+        case 6: return this.universityStore.studentsList;
+        default: return [];
+      }
+    },
+    updateUser(){
+      if(this.userRole === this.data.role) return;
+
+      //удаляем из прошлого списка
+      const list = this.getUserList(this.data.role);
+      for(let i = 0; i < list.length; i++){
+        if(list[i].id === this.data.id){
+          list.splice(i, 1);
+          break;
+        }
+      }
+      //добавляем в новый список
+      this.getUserList(this.userRole).push(<IUserGet>{
+        id: this.data.id,
+        name: this.data.name,
+        surname: this.data.surname,
+        thirdname: this.data.thirdname,
+        role: this.userRole,
+        faculty_id: this.data.faculty_id,
+        department_id: this.data.department_id,
+        educational_direction: this.data.educational_direction,
+      });
+      this.statusWindowStore.showStatusWindow(StatusCodes.success, `Должность изменена!`);
+    },  
+    deleteUser(){
+      const list = this.getUserList(this.data.role);
+      for(let i = 0; i < list.length; i++){
+        if(list[i].id === this.data.id){
+          list.splice(i, 1);
+          break;
+        }
+      }
+      this.statusWindowStore.showStatusWindow(StatusCodes.success, `Пользователь ${this.data.role === 6 ? 'отсчислен' : 'уволен'}!`);
     }
   }
 };
--- web/src/widgets/accountsCreateUsers.vue
+++ web/src/widgets/accountsCreateUsers.vue
@@ -44,11 +44,7 @@
           class="min-w-20 w-full md:w-auto max-w-none px-2 py-1 text-lg outline-none rounded-lg border-2 border-solid border-transparent focus:border-blue-800">
           
           <option class="text-lg" :value="-1" disabled>Выберите факультет</option>
-          <option class="text-lg" :value="2">{{getRolesName(2)}}</option>
-          <option class="text-lg" :value="3">{{getRolesName(3)}}</option>
-          <option class="text-lg" :value="4">{{getRolesName(4)}}</option>
-          <option class="text-lg" :value="5">{{getRolesName(5)}}</option>
-          <option class="text-lg" :value="6">{{getRolesName(6)}}</option>
+          <option v-for="item in getFaculties" class="text-lg" :value="item.id">{{ item.name }}</option>
         </select>
 
         <select 
@@ -57,11 +53,7 @@
           class="min-w-20 w-full md:w-auto max-w-none px-2 py-1 text-lg outline-none rounded-lg border-2 border-solid border-transparent focus:border-blue-800">
           
           <option class="text-lg" :value="-1" disabled>Выберите кафедру</option>
-          <option class="text-lg" :value="2">{{getRolesName(2)}}</option>
-          <option class="text-lg" :value="3">{{getRolesName(3)}}</option>
-          <option class="text-lg" :value="4">{{getRolesName(4)}}</option>
-          <option class="text-lg" :value="5">{{getRolesName(5)}}</option>
-          <option class="text-lg" :value="6">{{getRolesName(6)}}</option>
+          <option v-for="item in getDepartments" class="text-lg" :value="item.id">{{ item.name }}</option>
         </select>
 
         <input 
@@ -137,6 +129,7 @@
 <script lang="ts">
 import { mapStores } from 'pinia';
 import { useStatusWindowStore } from '@/stores/statusWindowStore';
+import { useUniversityStore } from '@/stores/universityStore';
 import { 
   ROLES_NAME, 
   INFO_FIELDS, 
@@ -163,7 +156,14 @@ export default {
     }
   },
   computed: {
-    ...mapStores(useStatusWindowStore),
+    ...mapStores(useStatusWindowStore, useUniversityStore),
+
+    getFaculties(){
+      return this.universityStore.facultiesList;
+    },
+    getDepartments(){
+      return this.universityStore.deparmentsList;
+    },
   },
   methods:{
     getRolesName(ind: number){
