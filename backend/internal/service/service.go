0706521527e5c53831c427f0b7180fd41e6520cf
Gewinn <daniilpostnickov@yandex.ru>
2024-11-28 11:49:02 +0300

    хотфиксик

--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -96,7 +96,6 @@ func NewService(repositories *repository.Repository, conf *config.Config) *Servi
 		DepartmentService:         NewDepartmentService(repositories.Department),
 		GpaService:                NewGpaService(repositories.Gpa),
 		AchievementService:        NewAchievementService(repositories.AchievementRepository),
-		WorkService:               NewWorkService(repositories.Work),
 		WorkService:               NewWorkService(repositories.Work, conf),
 	}
 }
