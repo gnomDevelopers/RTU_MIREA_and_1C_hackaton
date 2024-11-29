aeeab60718c87291e1e07c1f0d5b43b8e70fa7ca
unwelcome <moyapohtaxd@mail.ru>
2024-11-30 00:30:04 +0300

    сильнейшие мувы

--- web/src/stores/performancePageStore.ts
+++ web/src/stores/performancePageStore.ts
@@ -17,8 +17,7 @@ export const usePerformancePageStore = defineStore('performancePage', {
     async loadGroupGrades(){
       try{
         if(this.selectedGroup === null || this.selectedDiscipline === null) return;
-        const response: any = API_Grades_Group_Discipline_Get(this.selectedGroup, this.selectedDiscipline);
-
+        const response: any = await API_Grades_Group_Discipline_Get(this.selectedGroup, this.selectedDiscipline);
 
         const transformedData: IDataItem = transformData(response.data);
         console.log('added empty grades: ', transformedData);
